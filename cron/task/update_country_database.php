13946c4f707b2a41800369b57f1bae796a964384
Mark D. Hamill <mark@phpbbservices.com>
2019-07-31 22:17:10 -0400

    Cron fix, hopefully

--- cron/task/update_country_database.php
+++ cron/task/update_country_database.php
@@ -43,11 +43,14 @@ class update_country_database extends \phpbb\cron\task\base
 	{
 		// The Maxmind country database is updated weekly on Tuesdays. For our purposes, we'll assume a fresh database exists on Wednesdays.
 		// So to update the database, it must be on or after Wednesday and at least 7 days must have elapsed since the database was last updated.
+		// If for some reason the config variable phpbbservices_filterbycountry_cron_task_last_gc is set to 0, it's back in its initial state
+		// so the cron should fetch the database.
 
 		$todays_dow = date('w');	// 0 = Sunday, 6 = Saturday
-		$days_difference = floor((float) (time() - (int) $this->config['phpbbservices_filterbycountry_cron_task_last_gc'] / (24 * 60 * 60)));
+		$last_run = (int) $this->config['phpbbservices_filterbycountry_cron_task_last_gc'];
+		$days_difference = floor((float) ((time() - $last_run) / (24 * 60 * 60)));
 
-		return (bool) ($todays_dow >= 3 && $days_difference >= 7);
+		return (bool) (($todays_dow >= 3 && $days_difference >= 7) || ($last_run == 0));
 	}
 
 	public function run()
@@ -55,9 +58,12 @@ class update_country_database extends \phpbb\cron\task\base
 
 		// Updates the MaxMind country database via a phpBB cron
 
-		// Destroy current database, then download, ungzip, untar and stage an updated database. In cron mode, errors are placed in the admin log
-		// and the function will return true if it succeeds and false if an error happened.
-		return $this->helper->download_maxmind(true);
+		// Destroy current database, then download, ungzip, untar and stage an updated database. In cron mode, errors are placed in the admin log.
+		// An email may be sent to founders too.
+		$this->helper->download_maxmind(true);
+
+		// If for some reason downloading MaxMind fails, let's not lock up other crons. So let's always return true to cron.php to preclude this.
+		return true;
 
 	}
 
